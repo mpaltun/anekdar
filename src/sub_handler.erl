2879a7109723e8eb88fb524f638148c17a1e99d6
Mustafa Paltun <mpaltun@gmail.com>
2012-04-29 04:24:49 +0300

    follow cowboy's long polling impl

--- src/sub_handler.erl
+++ src/sub_handler.erl
@@ -1,19 +1,33 @@
 -module(sub_handler).
 -behaviour(cowboy_http_handler).
--export([init/3, handle/2, terminate/2]).
+-export([init/3, handle/2, info/3, terminate/2]).
 
-init({_Any, http}, Req, []) ->
-    {ok, Req, undefined}.
+-define(TIMEOUT, 30000).
 
-handle(Req, State) ->
+init({_Any, http}, Req, _Opts) ->
     {Channel, Req2} = cowboy_http_req:binding(channel, Req),
-    Pub_sub_mgr = pub_sub_manager:get_pid(),
-    Pub_sub_mgr ! {sub, Channel, self()},
-    receive
-        {ok, Message} ->
-            {ok, Req3} = cowboy_http_req:reply(200, [], Message, Req2)
-    end,
-    {ok, Req3, State}.
+    pub_sub_manager:sub(Channel),
+    {loop, Req2, undefined_state, ?TIMEOUT, hibernate}.
 
+handle(_Req, _State) ->
+    exit(badarg).
+
+info({ok, Message}, Req, State) ->
+    {ok, Req2} = cowboy_http_req:reply(200, [], Message, Req),
+    {ok, Req2, State};
+
+info(_Message, Req, State) ->
+    {loop, Req, State, hibernate}.
+
+%handle(Req, State) ->
+%    {Channel, Req2} = cowboy_http_req:binding(channel, Req),
+%    Pub_sub_mgr = pub_sub_manager:get_pid(),
+%    Pub_sub_mgr ! {sub, Channel, self()},
+%    receive
+%        {ok, Message} ->
+%            {ok, Req3} = cowboy_http_req:reply(200, [], Message, Req2)
+%    end,
+%    {ok, Req3, State}.
+%
 terminate(_Req, _State) ->
     ok.
