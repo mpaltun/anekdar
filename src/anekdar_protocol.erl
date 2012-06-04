371e4720a6dbd1fb3f63a96f1b88ec80ae331066
Mustafa Paltun <mpaltun@gmail.com>
2012-06-05 01:26:41 +0300

    unsubscribe protocol impl

--- src/anekdar_protocol.erl
+++ src/anekdar_protocol.erl
@@ -1,7 +1,8 @@
 -module(anekdar_protocol).
 -author('Mustafa Paltun <mpaltun@gmail.com>').
 
--export([parse/1, sub_response/3, pub_response/2, error_response/1, quit_response/1, ping_response/1]).
+-export([parse/1, sub_response/3, pub_response/2, unsub_response/1,
+            error_response/1, quit_response/1, ping_response/1]).
 
 -define(DELIMITER, " ").
 -define(ERROR, "-").
@@ -9,12 +10,16 @@
 -define(SUCCESS_INT, ":").
 
 -define(COMMAND_SUB, "sub").
+-define(COMMAND_UNSUB, "unsub").
 -define(COMMAND_PUB, "pub").
 -define(COMMAND_QUIT, "quit").
 -define(COMMAND_PING, "ping").
 
 sub_response(Channel, Message, Crlf) ->
     [?SUCCESS_STR, Channel, ?DELIMITER, Message, Crlf].
+
+unsub_response(Crlf) ->
+    [?SUCCESS_STR, <<"ok">>, Crlf].
 
 pub_response(Count, Crlf) ->
     [?SUCCESS_INT, list_to_binary(integer_to_list(Count)), Crlf].
@@ -40,6 +45,8 @@ parse(<<?COMMAND_PUB, ?DELIMITER, Data/binary>>) ->
         true ->
             {error, <<"message or channel missed">>} 
     end;
+parse(<<?COMMAND_UNSUB, ?DELIMITER, Channel/binary>>) ->
+    {unsub, Channel}; 
 parse(<<?COMMAND_PING, _Clrf/binary>>) ->
     {ping};
 parse(<<?COMMAND_QUIT, _Clrf/binary>>) ->
