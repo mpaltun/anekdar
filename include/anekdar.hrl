c2a370d54d6f4738788364b5a590e341370f04b8
challenge <mpaltun@gmail.com>
2012-06-14 00:10:44 +0300

    seperate constants

---
+++ include/anekdar.hrl
@@ -0,0 +1,11 @@
+-define(DELIMITER, " ").
+-define(ERROR, "-").
+-define(SUCCESS_STR, "+").
+-define(SUCCESS_INT, ":").
+-define(CRLF, "\r\n").
+
+-define(COMMAND_SUB, "sub").
+-define(COMMAND_UNSUB, "unsub").
+-define(COMMAND_PUB, "pub").
+-define(COMMAND_QUIT, "quit").
+-define(COMMAND_PING, "ping").
--- src/anekdar_tcp_handler.erl
+++ src/anekdar_tcp_handler.erl
@@ -4,17 +4,7 @@
 -export([start_link/4, stop/1]).
 -export([init/4]).
 
--define(DELIMITER, " ").
--define(ERROR, "-").
--define(SUCCESS_STR, "+").
--define(SUCCESS_INT, ":").
--define(CRLF, "\r\n").
-
--define(COMMAND_SUB, "sub").
--define(COMMAND_UNSUB, "unsub").
--define(COMMAND_PUB, "pub").
--define(COMMAND_QUIT, "quit").
--define(COMMAND_PING, "ping").
+-include("anekdar.hrl").
 
 -record(state, {
     socket :: inet:socket(),
