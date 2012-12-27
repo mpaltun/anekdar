60036972759b48c0be547ed8669f486432b558c9
Mustafa Paltun <mpaltun@gmail.com>
2012-12-28 01:26:59 +0200

    simple config file

--- src/anekdar.erl
+++ src/anekdar.erl
@@ -10,13 +10,14 @@ start() ->
 
 start(_Type, _Args) ->
   PrivDir = code:priv_dir(anekdar),
-  {ok, Dispatch} = file:consult(PrivDir ++ "/dispatch.conf"),
+  {ok, Conf} = file:consult(PrivDir ++ "/anekdar.conf"),
+  [{dispatch, Dispatch, http_address, Http_address, http_port, Http_port, tcp_address, Tcp_address, tcp_port, Tcp_port}] = Conf,
 	cowboy:start_listener(my_http_listener, 100,
-		cowboy_tcp_transport, [{port, 9999}],
-		cowboy_http_protocol, [{dispatch, Dispatch}]
+		cowboy_tcp_transport, [{port, Http_port}, {ip, Http_address}],
+		cowboy_http_protocol, [{dispatch, [Dispatch]}]
 	),
     cowboy:start_listener(my_tcp_listener, 100,
-        cowboy_tcp_transport, [{port, 9998}],
+        cowboy_tcp_transport, [{port, Tcp_port}, {ip, Tcp_address}],
         anekdar_tcp_handler, []
     ),
     ets_server:start_link(),
